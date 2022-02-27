99b14ef79c06df275b77f98e281f2b9bed76972c
Nirav-12345 <niravpandit0002@gmail.com>
2022-02-27 07:43:32 -0800

    adding Program file

---
+++ EmpOUC1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmpUC1ops
+{
+    public class EmpOUC1
+    {
+
+        public int s;
+        public int v;
+        public void u1c()
+
+        {
+            s = 1;
+            v = 0;
+
+            Random random = new Random();
+            int c = random.Next(2);
+
+            if ( c==1)
+            {
+                Console.WriteLine("Present");
+            }
+
+            else
+            {
+                Console.WriteLine("absent");
+            }
+        }
+
+    }
+}
