18b0292ed81d6431a2d4ea88bc4c2c90cc8f34b9
Nirav-12345 <niravpandit0002@gmail.com>
2022-02-27 07:57:26 -0800

    adding class file

--- Program.cs
+++ Program.cs
@@ -1,15 +1,18 @@
 ﻿using System;
 
-namespace EmpUC4Ops;
+namespace EUC5Ops;
 
 class Program
 {
-
     public static void Main(string[] args)
+
     {
-        u4c v = new u4c();
 
 
-        v.EU4c(1,2,20,0);
+
+        U5C l = new U5C();
+
+        l.eUC5(1, 2, 20, 20,0);
+
     }
 }
