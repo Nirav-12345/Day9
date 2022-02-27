726d30c6cb5ba4380c1da98f7aa3416edcc555c9
Nirav-12345 <niravpandit0002@gmail.com>
2022-02-27 07:42:32 -0800

    adding class file

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace EmpUC1ops;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        EmpOUC1 b = new EmpOUC1();
+
+        b.u1c();
+
+
+
+
+    }
+}
