92025ddf5ee0c3c95adf26524db2d9e7f0395865
Nirav-12345 <niravpandit0002@gmail.com>
2022-02-27 07:48:31 -0800

    adding Program file

---
+++ EmpUC2Ops.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmpUC2Oops;
+
+public class EmpUC2Ops
+{
+
+    public void u2c(int EmployeeWagePerHour, int FullDayWage)
+
+    {
+        int FullDayWage1 = FullDayWage;
+
+        int EmployeeWagePerHour2 = EmployeeWagePerHour;
+
+
+
+
+
+
+        Random random = new Random();
+        int num = random.Next(2);
+
+        if (num == 1)
+        {
+            float Calculate = (EmployeeWagePerHour2 * FullDayWage1);
+            Console.WriteLine("Per Day Wage" + Calculate);
+
+        }
+
+        else
+        {
+
+            int Calculate = 0;
+
+            Console.WriteLine("PerDay Wage" + Calculate);
+        }
+    }
+}
+
