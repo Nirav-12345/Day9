e0419f7b62b1fdd6c85cd7c72c0ca575a61a097f
Nirav-12345 <niravpandit0002@gmail.com>
2022-02-28 10:27:44 -0800

    adding program file

---
+++ Addresbook1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Addressbook2
+{
+    public class addressBook
+    {
+
+        public void d1()
+        {
+
+            string FirstName = Console.ReadLine();
+
+            string LastName = Console.ReadLine();
+
+            string PhoneNuber = Console.ReadLine();
+
+            string Addresses = Console.ReadLine();
+
+            double contact = Convert.ToInt32(Console.ReadLine());
+           
+        }
+    }
+
+
+
+
+
+
+
+    public class A : addressBook
+    {
+        public void d2()
+        {
+            string FirstName1 = Console.ReadLine();
+
+            string LastName1 = Console.ReadLine();
+
+            string PhoneNuber1 = Console.ReadLine();
+
+            string Addresses1 = Console.ReadLine();
+
+            double contact1 = Convert.ToInt32(Console.ReadLine());
+        }
+    }
+}
+        
+
+
+    
+
+
+
+
+
